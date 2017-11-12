02ab8ba1c556d523bf78baaa918623a8d2be03fc
kuroshikick <hahahathai@gmail.com>
2017-11-12 11:03:50 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -130,13 +130,13 @@ class LINE extends Command {
         this.command('kamu siapa', this.getProfile.bind(this));
         this.command('.status', `Your Status: ${JSON.stringify(this.stateStatus)}`);
         this.command(`.left ${payload}`, this.leftGroupByName.bind(this));
-        this.command('.speed', this.getSpeed.bind(this));
+        this.command('fast8', this.getSpeed.bind(this));
         this.command('.kernel', this.checkKernel.bind(this));
         this.command(`kick ${payload}`, this.OnOff.bind(this));
         this.command(`cancel ${payload}`, this.OnOff.bind(this));
         this.command(`qrp ${payload}`, this.OnOff.bind(this));
-        this.command(`.kickall ${payload}`,this.kickAll.bind(this));
-        this.command(`.cancelall ${payload}`, this.cancelMember.bind(this));
+        this.command(`หาหมี ${payload}`,this.kickAll.bind(this));
+        this.command(`เชินทำไม ${payload}`, this.cancelMember.bind(this));
         this.command(`.set`,this.setReader.bind(this));
         this.command(`.recheck`,this.rechecks.bind(this));
         this.command(`.clearall`,this.clearall.bind(this));
